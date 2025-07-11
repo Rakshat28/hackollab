a4643ae2e1dbad001e3be6ed1de1beccdee6f270
Rakshat28 <rakshat07ps@gmail.com>
2025-07-12 02:55:03 +0530

    commit summary logic

---
+++ src/lib/github.ts
@@ -0,0 +1,103 @@
+import { Octokit } from "octokit";
+import { db } from "~/server/db";
+import axios from 'axios';
+import { getCommitSummary } from "./gemini";
+
+export const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+type Response = {
+  commitMessage: string;
+  commitHash: string;
+  commitAuthorName: string;
+  commitAvatar: string;
+  commitDate: string;
+  commitSummary: string;
+};
+
+export const getCommitHashes = async (githubUrl: string): Promise<Response[]> => {
+  const [, , , owner, repo] = githubUrl.split("/");
+
+  if(!owner || !repo){
+    throw new Error("owner or repo missing")
+  }
+  const { data } = await octokit.rest.repos.listCommits({
+    owner : owner,
+    repo : repo, 
+  });
+
+const commits: Response[] = data.map((commit) => {
+  return {
+    commitMessage: commit.commit.message || "",
+    commitHash: commit.sha,
+    commitAuthorName: commit.commit.author?.name ?? "Unknown",
+    commitAvatar: commit.author?.avatar_url ?? "",
+    commitDate: commit.commit.author?.date ?? "",
+    commitSummary: commit.commit.message?.split("\n")[0] ?? "",
+  };
+});
+
+  return commits.slice(0,10).sort((a, b) => new Date(b.commitDate).getTime() - new Date(a.commitDate).getTime());
+};
+
+export const pollCommits = async (projectId: string) => {
+    const {githubUrl} = await fetchProjectGithubUrl(projectId)
+    const commitHashes = await getCommitHashes(githubUrl)
+    const unprocessedCommits = await filterUnprocessedCommits(projectId, commitHashes)
+    const summaryResponses = await Promise.allSettled(unprocessedCommits.map((unprocessedCommit)=>summariseCommit(githubUrl,unprocessedCommit.commitHash)));
+    const summaries = summaryResponses.map((summary)=>{
+      if(summary.status === 'fulfilled'){
+        return summary.value
+      }
+      return "";
+    })
+    const commits = await db.commit.createMany({
+      data: summaries.map((summary,index)=>{
+        return {
+          projectId : projectId,
+          commitHash : unprocessedCommits[index]!.commitHash,
+          commitMessage : unprocessedCommits[index]!.commitMessage,
+          commitAuthorName : unprocessedCommits[index]!.commitAuthorName,
+          commitAvatar : unprocessedCommits[index]!.commitAvatar,
+          commitDate : unprocessedCommits[index]!.commitDate,
+          commitSummary : summary
+        }
+      })
+    })
+    return commits;
+}
+
+async function summariseCommit(githubUrl: string, commitHash: string){
+  const {data} = await axios.get<string>(`${githubUrl}/commit/${commitHash}.diff`,{
+    headers: {
+      Accept : 'application/vnd.github.v3.diff'
+    }
+  })
+  return await getCommitSummary(data) || "";
+}
+
+async function fetchProjectGithubUrl(projectId: string){
+    const project = await db.project.findUnique({
+        where: {
+            id: projectId
+        },
+        select: {
+            githubUrl : true
+        }
+    })
+    if(!project?.githubUrl){
+        throw new Error('Project has no GitHub url')
+    }
+    return { githubUrl : project.githubUrl}
+}
+
+async function filterUnprocessedCommits(projectId: string, commitHashes: Response[]){
+    const processedCommits = await db.commit.findMany({
+        where: {
+            projectId: projectId
+        }
+    })
+    const unprocessedCommits = commitHashes.filter((commit) => !processedCommits.some((processedCommit)=> processedCommit.commitHash === commit.commitHash));
+    return unprocessedCommits;
+}
